6c66a8222bd953f21b348d886ceb556b8b439d6e
davis009 <davis6247@gmail.com>
2018-02-19 01:41:00 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -17,44 +17,7 @@ module.exports = function zeros(expression) {  //  //Expression contains only fa
 
     return zeros;
 }
-
-
-
-
-function multiply(first, second){   //both String
-  if(typeof(first) != "string" || typeof(second) != "string" ||
-  isNaN(first) || isNaN(second))  return false;
-
-  if(first.length < 7 && second.length < 7) return (first * second).toString();
-
-  first = first.split('').reverse();
-  second = second.split('').reverse();
-    var result = [];
-
-    for (var i = 0; i < first.length; i++) {      //iterate over first operand
-        for (var j = 0; j < second.length; j++) {    //iterate over second operand
-            if (result[i + j] == undefined) {    //create slot in the [result]    
-                result[i + j] = 0;
-            }
-
-            result[i + j] += first[i] * second[j];
-        }
-    }
-
-    for (var i = 0; i < result.length; i++) {   //summing up all number in the [result] with carry over
-        if (result[i] >= 10) {
-            if (result[i + 1] == undefined) {   //create slot in the [result] 
-                result[i + 1] = 0;
-            }
-
-            result[i + 1] += Math.floor(result[i] / 10);
-            result[i] %= 10;
-        }
-    }
-
-    return result.reverse().join('');
-}
-//calculating factorial for very big numbers
+//calculating factorial for very big numbers using function from previous task
 function bigFactorial(number, config){   //config = str where "!" stand for usual factorial, and "!!"
                                       //stand for factorial through one like "10!! === 2 * 4 * 6 * 8 * 10`"
   var phase  = config == "!" ? 1 : 2;   //config should be "!" either "!!"
@@ -67,28 +30,36 @@ function bigFactorial(number, config){   //config = str where "!" stand for usua
   return summ;
 }
 
-
-//first ateemp to solve it more efficiently, 
-
-//phase - string, comes "!" either "!!",  where "!" stand for usual factorial, and "!!"
-//stand for factorial through one like "10!! === 2 * 4 * 6 * 8 * 10`"
-function zerosInFactorial(number, phase){   
-    var zeros = 0;
-    if(phase == "!"){
-        zeros += Math.floor(number / 5)     
-        zeros += Math.floor(number / 25);  
-
-        return zeros;
-    }
-    else if(phase == "!!"){
-        if(number % 2 == 0){
-            zeros += Math.floor(number / 10);
-            zeros += Math.floor(number / 50);
-            return zeros;
-        }
-        return zeros;
-        
-    }
-
-    return -1;
-}
+function multiply(first, second){   //both String
+    if(typeof(first) != "string" || typeof(second) != "string" ||
+    isNaN(first) || isNaN(second))  return false;
+  
+    if(first.length < 7 && second.length < 7) return (first * second).toString();
+  
+    first = first.split('').reverse();
+    second = second.split('').reverse();
+      var result = [];
+  
+      for (var i = 0; i < first.length; i++) {      //iterate over first operand
+          for (var j = 0; j < second.length; j++) {    //iterate over second operand
+              if (result[i + j] == undefined) {    //create slot in the [result]    
+                  result[i + j] = 0;
+              }
+  
+              result[i + j] += first[i] * second[j];
+          }
+      }
+  
+      for (var i = 0; i < result.length; i++) {   //summing up all number in the [result] with carry over
+          if (result[i] >= 10) {
+              if (result[i + 1] == undefined) {   //create slot in the [result] 
+                  result[i + 1] = 0;
+              }
+  
+              result[i + 1] += Math.floor(result[i] / 10);
+              result[i] %= 10;
+          }
+      }
+  
+      return result.reverse().join('');
+  }
